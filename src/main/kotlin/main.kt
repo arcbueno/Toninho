a8b2f3a53c786522e3b1e160bf55a824dd3c5eab
Pedro Bueno <pedro.bueno@neovero.com>
2021-03-22 23:16:04 -0300

    first release

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,71 @@
+import com.jessecorbett.diskord.api.model.UserStatus
+import com.jessecorbett.diskord.api.websocket.model.ActivityType
+import com.jessecorbett.diskord.api.websocket.model.UserStatusActivity
+import com.jessecorbett.diskord.dsl.bot
+import com.jessecorbett.diskord.dsl.command
+import com.jessecorbett.diskord.dsl.commands
+import kotlin.random.Random
+
+const val BOT_TOKEN = "ODIzNjc5OTQ4MjUwMjg0MDcy.YFkVug.2Bg47rq4oWzujDivTKvE1RgwNY0"
+const val PERMISSION_INTEGER = 2148005952
+
+suspend fun main() {
+    val links = Links()
+    bot(BOT_TOKEN) {
+        // . as default prefix
+        var guildList = this.clientStore.guilds;
+        started {
+            setStatus(UserStatus.ONLINE, activity = UserStatusActivity("Ape Out", ActivityType.GAME));
+        }
+        commands {
+            command("mamaco") {
+                var mamacoList = links.linkMamaco
+                reply(mamacoList[getRandomIndex(mamacoList.size)])
+            }
+            command("zap") {
+                var zapList = links.linkZap
+                reply(zapList[getRandomIndex(zapList.size)])
+            }
+            command("genshin") {
+                var genshinList = links.linkGenshin
+                reply(genshinList[getRandomIndex(genshinList.size)])
+            }
+            command("flip") {
+                delete()
+                reply(links.monkyFlip)
+            }
+            command("top") {
+                delete()
+                reply("TOPZERA IRMÃO :ok_hand: :ok_hand: :ok_hand: :ok_hand: :ok_hand: :rofl: :rofl:")
+            }
+            command("violencia") {
+                delete()
+                var guild = guildList.get(this.guildId!!);
+                var guildMembers = guild.getMembers(1000);
+                var currentIndex = getRandomIndex(guildMembers.size)
+                reply("Vô dale em ${guildMembers[currentIndex].user!!.username}");
+            }
+            command("violencia") {
+                delete()
+                var guild = guildList.get(this.guildId!!);
+                var guildMembers = guild.getMembers(1000);
+                var currentIndex = getRandomIndex(guildMembers.size)
+                reply("Vô dale em ${guildMembers[currentIndex].user!!.username}");
+            }
+            command("disputa") {
+                delete()
+                var guild = guildList.get(this.guildId!!);
+                var guildMembers = guild.getMembers(1000);
+                var currentIndex = getRandomIndex(guildMembers.size)
+                reply("Bicho, quem ta certo é ${guildMembers[currentIndex].user!!.username}");
+            }
+            command("toninho") {
+                reply("UHHHUHHH AAAA AAAHHHH UHUUHU UGA UHHHHH (gritos de mamaco)");
+            }
+        }
+    }
+}
+
+fun getRandomIndex(maxLength: Int): Int {
+    return Random.nextInt(0, maxLength);
+}
